fac64879fb0a0476325496def43f0bc45f642d87
Longmoon <chris@longmoon.com>
2015-03-29 16:00:36 -0700

    Add sample questions
    Added sample questions, organized data, and set up an ngRepeat to show
    questions on page.

--- js/quickAuditApp.js
+++ js/quickAuditApp.js
@@ -4,5 +4,25 @@ quickAudit.controller("quickAuditController", function($scope, $firebaseObject) 
   var quickAuditDataRef = new Firebase("https://quickaudit.firebaseio.com");
   var quickAuditSyncObject = $firebaseObject(quickAuditDataRef);
 
-  quickAuditSyncObject.$bindTo($scope, "data");
+  quickAuditSyncObject.$bindTo($scope, "quickAudit");
 });
+
+
+
+/* Sample Questions
+
+[
+	{
+		q: "What do you think?",
+		a: null
+	},
+		{
+		q: "What do you know?",
+		a: null
+	},	{
+		q: "What have you heard?",
+		a: null
+	}
+]
+
+*/
