d108cd8ca16a4440563325da5c0501e0481fa0de
Longmoon <chris@longmoon.com>
2015-04-12 11:52:13 -0700

    Wire 'submit' btn to Firebase
    Post a hardcoded response to Firebase when user hits 'submit' button.

--- js/quickAuditApp.js
+++ js/quickAuditApp.js
@@ -2,17 +2,19 @@ var quickAudit = angular.module("quickAudit", ["firebase"]);
 
 quickAudit.controller("quickAuditController", function($scope, $firebaseObject) {
 	var quickAuditDataRef = new Firebase("https://quickaudit.firebaseio.com");
-	var quickAuditSyncObject = $firebaseObject(quickAuditDataRef);
+	var quickAuditSyncArray = $firebaseObject(quickAuditDataRef);
 	
-	quickAuditSyncObject.$bindTo($scope, "quickAudit");
+	quickAuditSyncArray.$bindTo($scope, "quickAudit");
+	var response = quickAuditDataRef.child("responses");
 	
 	$scope.started = false;
-	
 	$scope.start = function() {
 		$scope.started = true;
 	}
-	
 	$scope.submit = function() {
 		$scope.started = false;
+		response.push({
+			question1: "yes"
+		});
 	}
 });
